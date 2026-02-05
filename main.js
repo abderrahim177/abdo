4a28f6db89ea7ee12dd64a484139b1838eda54c2
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-02-05 23:28:15 +0100

    chenge

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ let first_name =document.getElementById('First_name');
 let last_name =document.getElementById('last_name');
 let age =document.getElementById('age');
 let btn = document.getElementById('btn');
+let count = document.getElementById('count');
 // variables of table
-
-
-
 let arr = [];
 
 
@@ -16,14 +14,25 @@ let arr = [];
         second_name : last_name.value,
         age_user : age.value,
     };
-
+    
     arr.push(obj);
     localStorage.setItem('users',JSON.stringify(arr));
-    
+    localStorage.getItem()
     showdata()
     clearData()
 }
-
+btn.onclick =  function (){
+    let time = Number(count.value) || 1;
+    for (let i = 0 ; i< time; i++){
+        arr.push({
+            nom : first_name.value,
+            second_name : last_name.value,
+            age_user : age.value,
+        })
+    }
+    showdata();
+    clearData();
+}
 function showdata(){
     let table = '';
      for (let i = 0 ; i< arr.length ; i++){
@@ -33,12 +42,13 @@ function showdata(){
     <td>${arr[i].nom}</td>
     <td>${arr[i].second_name}</td>
     <td>${arr[i].age_user}</td>
-    <td onclick ="update_data()" id="update">update</td>
-    <td onclick = "delete_Data()" id="delete">delete 🗑</td>
+    <td >update 🖍</td>
+    <td onclick = "delete_Data(${i})" >delete 🗑</td>
     </tr>
     
      `;
      }
+     
    document.getElementById('tbody').innerHTML = table;
 
 }
@@ -47,8 +57,14 @@ function clearData(){
     first_name.value = '';
     last_name.value = '';
     age.value = '';
+    count.value = '';
+};
+let button = document.getElementById('delete');
+ function delete_Data(index){
+    arr.splice(index , 1);
+    showdata();
 }
-
-
-
+ let search_bt_name = document.getElementById('search_by_name');
+ let search_bt_age = document.getElementById('search_by_age');
+ let delete_All = document.getElementById('delete_All');
 
